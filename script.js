3ce8e2beb7edd5f03b8fd90e55de5d49df556ef5
annielcook <anniecook@ostby.com>
2015-05-23 16:35:56 -0700

    setup for js

---
+++ script.js
@@ -0,0 +1,11 @@
+//generate random number
+var num = Math.floor((Math.random() * 100) + 1);
+//if guesses left -- get guess
+
+//subtract one from counter
+
+//tell user if high or low or right
+
+// repeat
+
+
