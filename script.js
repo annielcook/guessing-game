6c0df6fd589b25adf9e0dfff5f51f0859a7df196
annielcook <anniecook@ostby.com>
2015-05-26 20:47:24 -0700

    cleaned code by makign repeats into functions

--- script.js
+++ script.js
@@ -3,125 +3,116 @@ var target;
 var input;
 var arr = [];
 
-
 function compare() {
-  //$("h2").remove();
-  guesses--;
-  $('input').val('');
-  if(target < input){
+  if (target == input){
+    feedback("<h2>CORRECT!</h2>", "red");
+    $(".guesses_left").remove();
+  } else if (guesses == 0){
+    $( ".prompt" ).append( "<h2>Game Over! The number was " + target + ".</h2>" );
+    clickClear();
+  } else if(target < input){
     if((input - target) > 25){
-      $(".prompt").append("<h2>Ice cold! "+ input + " is too high</h2>" );
-      $("h2").css("color", "blue");
-      $("input").css("border", "5px solid blue");
+      feedback("<h2>Ice cold! "+ input + " is too high</h2>", "blue");
     } else {
-      $(".prompt").append("<h2>A tad chilly! "+ input + " is a little too high</h2>" );
-      $("h2").css("color", "purple");
-      $("input").css("border", "5px solid purple");
+      feedback("<h2>A tad chilly! "+ input + " is a little too high</h2>", "purple")
     }
   } else if (target > input) {
     if((target - input) > 25){
-      $(".prompt").append("<h2>Ice cold! "+ input + " is too low</h2>" );
-      $("h2").css("color", "blue");
-      $("input").css("border", "5px solid blue");
+      feedback("<h2>Ice cold! "+ input + " is too low</h2>", "blue");
     } else {
-      $(".prompt").append("<h2>A tad chilly! "+ input + " is a little too low</h2>" );
-      $("h2").css("color", "purple");
-      $("input").css("border", "5px solid purple");
+      feedback("<h2>A tad chilly! "+ input + " is a little too low</h2>", "purple")
     }
-  } else if (target == input){
-    $(".prompt").append("<h2>"+ input + " is correct!</h2>" );
-    $("h2").css("color", "red");
-    $("input").css("border", "5px solid red");
-  } else {
-    console.log("undefined input")
   }
 };
 
-function clear(){
+function clickClear(){
   guesses = 5;
   $("input").css("border", "5px solid #676767");
-  $("input").val("");
+  $("input").val("Guess a number between 1 and 100");
   arr = [];
+  target = Math.floor((Math.random() * 100) + 1);
 };
 
 function hint(){
   $("h2").remove();
-  var add = Math.floor(Math.random() * 5) + 1
-  var sub = Math.floor(Math.random() * 5) + 1
-  var lower = target - sub;
-  var higher = target + add;
-  $(".prompt").append("<h2>The number is between "+ lower + " and " + higher + ".</h2>" );
-  $("h2").css("color", "#676767");
+  var lower = target - Math.floor(Math.random() * 5) + 1;
+  var higher = target + Math.floor(Math.random() * 5) + 1;
+  feedback("<h2>The number is between "+ lower + " and " + higher + ".</h2>", "#676767")
   $("h2").css("font-style", "italic");
-  $("input").css("border", "5px solid #676767");
 };
 
 
-
-$(document).ready(function(){
-  target = Math.floor((Math.random() * 100) + 1);
-
+function clearBox(){
+  //clear value text of input field when clicked
   $("input").on("focus", function(){
     $("input").val("");
   });
 
+  //clear input field when click submit
   $(".sub").on("click", function(event){
     $("input").val("");
   });
+};
 
+function updateGuesses(){
   $('.guesses_left').html(guesses + " guesses left");
+};
 
+function feedback(prompt, color){
+  $( ".prompt" ).append(prompt);
+  $("h2").css("color", color);
+  $("input").css("border", "5px solid " + color);
+};
+
+function submitGuess(){
+  $("h2").remove();
+  input = $('input').val();
+  $("input").val("");
+
+  if(guesses === 0){
+    $( ".prompt" ).append( "<h2>Game Over! The number was " + target + ".</h2>" );
+    clickClear();
+  } else if ($.inArray(input, arr) !== -1){
+    feedback("<h2>"+ input + " has already been entered. Try a different number.</h2>", "#676767");
+  } else if (input >= 1 && input <= 100){
+    guesses--;
+    arr.push(input);
+    compare();
+    updateGuesses();  
+  } else {
+    feedback("<h2>Please enter a number between 1 and 100</h2>", "#676767");
+  }
+};
+
+
+$(document).ready(function(){
+
+  target = Math.floor((Math.random() * 100) + 1);
+  clearBox();
+  updateGuesses();
+
+  //if enter is clicked submit
   $('#box').keydown(function(event){    
     if(event.keyCode==13){
        $('#sub').trigger('click');
     }
   });
 
+  //if clear is clicked
   $('#clear').click(function(){
-    clear();
+    clickClear();
     $('.guesses_left').html(guesses + " guesses left");
     $("h2").remove();
   });
 
+  //if hint is clicked
   $('#hint').click(function(){
     hint();
   });
 
+  //if submit is clicked
   $('#sub').click(function () {
-    $("h2").remove();
-    input = $('input').val();
-    if(guesses === 0){
-      $( ".prompt" ).append( "<h2>Game Over!</h2>" );
-      clear();
-    }else if(input == '' || input > 100 || input < 1){
-      $( ".prompt" ).append( "<h2>Please enter a number between 1 and 100</h2>" );
-      $("input").val("");
-    } else if ($.inArray(input, arr) !== -1){
-      $( ".prompt" ).append( "<h2>"+ input + " has already been entered. Try a different number.</h2>" );
-      $("h2").css("color", "#676767");
-      $("input").val("");
-      $("input").css("border", "5px solid #676767");
-    } else {
-      arr.push(input);
-      compare();
-      console.log(guesses);
-    }
-    $('.guesses_left').html(guesses + " guesses left");
+    submitGuess();
   });
-  //  input = $('input').val();
-    
-  //  $('.button').on("click", compare(guesses));
-  //  console.log(guesses);
-
-
-  //  //});
-  
-//if guesses left -- get guess
-
-//subtract one from counter
-
-//tell user if high or low or right
-
-// repeat
 
 });
